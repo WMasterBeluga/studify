7c165322b3518702594f4a2be6f3c4db5ace8b29
bnpm63 <150587691+bnpm63@users.noreply.github.com>
2024-10-26 22:34:47 -0700

    Changed chat layout

--- studify/src/app/chat/page.tsx
+++ studify/src/app/chat/page.tsx
@@ -5,11 +5,11 @@ import Navbar from "/src/app/Navbar.tsx";
 export default function Home() {
   return (
     <div className="flex justify-center">
-      <div className="max-w-screen-2xl w-full px-20">
+      <div className="max-w-screen-2xl w-full px-4">
         <div className="w-full"></div>
-
-        <div className="mt-5">
           <Navbar />
+        <div className="mt-5">
+          
 
           {/* Neutral Box Starts Here */}
           <div className="flex justify-center mt-8">
--- studify/src/app/features/page.tsx
+++ studify/src/app/features/page.tsx
@@ -4,11 +4,11 @@ import Navbar from "/src/app/Navbar.tsx";
 export default function Home() {
   return (
     <div className="flex justify-center">
-      <div className="max-w-screen-2xl w-full px-20">
+      <div className="max-w-screen-2xl w-full px-4">
         <div className="w-full"></div>
-
-        <div className="mt-5">
           <Navbar />
+        <div className="mt-5">
+
 
         </div>
       </div>
