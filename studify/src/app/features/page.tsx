0a7f15a041a2e5774f90f53f8c253885b5f049df
bnpm63 <150587691+bnpm63@users.noreply.github.com>
2024-10-26 22:39:10 -0700

    Changed navbar layout

--- studify/src/app/features/page.tsx
+++ studify/src/app/features/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "/src/app/Navbar.tsx";
 export default function Home() {
   return (
     <div className="flex justify-center">
-      <div className="max-w-screen-2xl w-full px-4">
+      <div className="max-w-screen-2xl w-full px-4 lg:px-20">
         <div className="w-full"></div>
           <Navbar />
         <div className="mt-5">
